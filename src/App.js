187c00270442cb2cc841de1bdbd0ebfa4425ad83
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-02 00:59:24 +0200

    Introduce state to main App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'moss', age: 17 },
+      { name: 'tree', age: 104 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
-        <Person name="moss" age="17"/>
-        <Person name="tree" age="104">Hobbies: Producing oxygen</Person>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Producing oxygen</Person>
       </div>
     );
   }
