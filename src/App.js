6e061da547c574004b03852e5976a1761eb0c428
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-02 01:15:25 +0200

    Introduce setState and onClickHandler to main App component

--- src/App.js
+++ src/App.js
@@ -10,11 +10,22 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // DO NOT DO THIS
+    // this.state.persons[0].name = "Bobo";
+
+    // DO THIS INSTEAD
+    this.setState({persons: [
+      { name: 'Bobo', age: 17 },
+      { name: 'tree', age: 104 }
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Producing oxygen</Person>
       </div>
