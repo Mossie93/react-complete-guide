090421c7290aabb234e50f205873592a37bfa976
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-02 13:47:11 +0200

    Revert "Excursion to hooks world"
    This reverts commit 944b761afa8ff6277de2b1926ba68bebc892ecbd.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'moss', age: 17 },
       { name: 'tree', age: 104 }
     ]
-  });
+  }
 
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // DO NOT DO THIS
     // this.state.persons[0].name = "Bobo";
-  
+
     // DO THIS INSTEAD
-    setPersonsState({persons: [
+    this.setState({persons: [
       { name: 'Bobo', age: 17 },
       { name: 'tree', age: 104 }
     ]})
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React app!</h1>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].name}>Hobbies: Producing oxygen</Person>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app!</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Producing oxygen</Person>
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
