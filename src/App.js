944b761afa8ff6277de2b1926ba68bebc892ecbd
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-02 13:47:08 +0200

    Excursion to hooks world
    Remember that useState function does not merges different states

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'moss', age: 17 },
       { name: 'tree', age: 104 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // DO NOT DO THIS
     // this.state.persons[0].name = "Bobo";
-
+  
     // DO THIS INSTEAD
-    this.setState({persons: [
+    setPersonsState({persons: [
       { name: 'Bobo', age: 17 },
       { name: 'tree', age: 104 }
     ]})
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React app!</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Producing oxygen</Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React app!</h1>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].name}>Hobbies: Producing oxygen</Person>
+    </div>
+  );
 }
 
-export default App;
+export default app;
