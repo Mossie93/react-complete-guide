9e418517dbc74d78167dc6a308c07a93ed66e5ec
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-09 17:03:02 +0200

    Use styled-components package to style Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import './Person.css';
+// import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  border: 1px solid black;
+  padding: 20px;
+  width: 60%;
+  margin: 16px auto;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
+
 
 const person = (props) => {
   return (
-    <div className="Person">
+    // <div className="Person">
+    <StyledDiv>
       <p onClick={props.click}>I'm a {props.name}, I'm {props.age} years old</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}></input>
-    </div>
+    </StyledDiv>
   )
 }
 
