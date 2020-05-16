e400b1152ada91b48295ee66fa126f60d5fe3e7a
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-16 23:15:40 +0200

    Use build0-in higher order component - React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
-import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
 
     return (
-      <Aux>
+      <Fragment>
         <p onClick={this.props.click}>I'm a {this.props.name}, I'm {this.props.age} years old</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-      </Aux>
+      </Fragment>
     );
   };
 };
