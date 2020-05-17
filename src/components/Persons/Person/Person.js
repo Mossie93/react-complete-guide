9e29c17958927afbc4c8e8dc64d7566496dea1d0
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-18 01:01:46 +0200

    Refactor context in class-based component
    The new way  gives us access to the context from outside render function

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,6 +11,8 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     this.inputElementRef.current.focus();
   }
@@ -20,9 +22,7 @@ class Person extends Component {
 
     return (
       <Fragment>
-        <AuthContext.Consumer>
-          {(context) => context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
-        </AuthContext.Consumer>
+        {this.context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
         <p onClick={this.props.click}>I'm a {this.props.name}, I'm {this.props.age} years old</p>
         <p>{this.props.children}</p>
         <input 
