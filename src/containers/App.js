1517f0b7a75b7a5acd3fe45aef19c05d0adb107c
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-17 02:46:07 +0200

    Add higher order component with classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -86,7 +87,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({showCockpit: false})}}>
           Remove Cockpit
         </button>
@@ -96,7 +97,7 @@ class App extends Component {
           showPersons={this.state.showPersons}
         /> : null }
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
