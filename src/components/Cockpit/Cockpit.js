d0c129f3ad77ac2c416fbe9c92f2858dc494fe3f
Rafal Mossakowski <rmossakowski@gmail.com>
2020-05-18 01:37:52 +0200

    Refactor function-based  component to use useContex thook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.css';
 import AuthContext from  '../../context/auth-context';
 
 const cockpit = (props) =>  {
   const toggleButtonRef = useRef(null);
+  const authContext = useContext(AuthContext);
   let btnClass = [''];
   let assignedClasses = [];
 
@@ -48,9 +49,7 @@ const cockpit = (props) =>  {
       >
         Toggle persons
       </button>
-      <AuthContext.Consumer>
-        {(context) => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
